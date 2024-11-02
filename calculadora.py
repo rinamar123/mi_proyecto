0a8477f02c06fa222e41f22513a5e17b05d900aa
Luisdiaz-19 <freeluiscrakc@gmail.com>
2024-11-02 03:38:36 -0500

    proyecto calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-#luis jose diaz urieles
+#luis jose diaz urieles y rina marriaga
 #31/10/2024
 #grupo 2 semestre 5
 
@@ -35,13 +35,13 @@ def division():
 root = tk.Tk()
 root.title("Calculadora")
 
+# Creación de los widgets
 tk.Label(root, text="Primer número:").grid(row=0, column=0)
-tk.Label(root, text="Segundo número:").grid(row=1, column=0)
-
 entry1 = tk.Entry(root)
-entry2 = tk.Entry(root)
-
 entry1.grid(row=0, column=1)
+
+tk.Label(root, text="Segundo número:").grid(row=1, column=0)
+entry2 = tk.Entry(root)
 entry2.grid(row=1, column=1)
 
 tk.Button(root, text="+", command=suma).grid(row=2, column=0)
@@ -52,5 +52,6 @@ tk.Button(root, text="/", command=division).grid(row=3, column=1)
 resultado = tk.StringVar()
 tk.Label(root, textvariable=resultado).grid(row=4, column=0, columnspan=2)
 
+# Inicio del bucle de la aplicación
 root.mainloop()
 
